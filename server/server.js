2c6fc5819d0514f191e36fc855d3d5c5550fb21b
Deepika Rani <deepikasingh3729@gmail.com>
2025-08-26 17:55:08 +0530

    testing

--- server/server.js
+++ server/server.js
@@ -21,3 +21,4 @@ app.use("/api/status", (req, res) => res.send("Server is live"));
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
+console.log("testing server");
