22c2acc11dafb8c6eec7f0e2b874e01231880dd1
Deepika Rani <deepikasingh3729@gmail.com>
2025-09-06 19:50:39 +0530

    feat: add text & image send in chat

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,10 +1,41 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import assets, { messagesDummyData } from '../assets/assets'
 import { formatMessageTime } from '../lib/utils'
+import { ChatContext } from '../../context/ChatContext'
+import { AuthContext } from '../../context/AuthContext'
 
-const ChatContainer = ({ selectedUser, setSelectedUser }) => {
+const ChatContainer = () => {
+  const {messages, selectedUser, setSelectedUser, sendMessage,
+    getMessages} = useContext(ChatContext)
+
+  const { authUser, onlineUsers } = useContext(AuthContext)
 
   const scrollEnd= useRef()
+
+  const [input, setInput] = useState('');
+
+  //Handle sending a message
+  const handleSendMessage =async (e) =>{
+    e.preventDefault();
+    if(input.trim() ==="") return null;
+    await sendMessage({text: input.trim()});
+    setInput("")
+  }
+
+  //Handle sending an image 
+  const handleSendImage= async (e) =>{
+    const file =e.target.files[0];
+    if(!file || !file.type.startsWith("image/")){
+      toast.error("select an image file")
+      return;
+    }
+    const reader =new FileReader();
+    reader.onloadend =async ()=>{
+      await sendMessage({image: reader.result})
+      e.target.value =""
+    }
+    reader.readAsDataURL(file)
+  }
 
   useEffect(()=>{
     if(scrollEnd.current){
@@ -67,16 +98,18 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
       {/* Bottom sec */}
       <div className='absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3'>
         <div className='flex-1 flex items-center bg-gray-100/12 px-3 rounded-full'>
-          <input type="text" placeholder="send a message"
+          <input onChange ={(e)=> setInput(e.target.value)} value ={input}
+          onKeyDown={(e)=> e.key ==="Enter" ? handleSendMessage(e) : null}
+          type="text" placeholder="send a message"
            className='flex-1 text-sm p-3 border-none rounded-lg outline-none
            text-white placeholder-gray-400'/>
-          <input type="file"  id='image' accept='image/png, image/jpeg' hidden/>
+          <input onChange={handleSendImage} type="file"  id='image' accept='image/png, image/jpeg' hidden/>
           <label htmlFor="image"> 
             <img src={assets.gallery_icon} alt=""  className="w5 mr-2
             cursor-pointer"/>
           </label>
         </div>
-        <img src={assets.send_button} alt="" className="w-7 cursor-pointer" />
+        <img onClick={handleSendMessage} src={assets.send_button} alt="" className="w-7 cursor-pointer" />
 
       </div>
 
